d2d213e357ef082660644344791b06bf72266b6c
Gonzalo J. Osende <gonzalojose.osende.perez@alumnos.upm.es>
2016-05-04 20:09:40 +0200

    Tema 3: Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var controllers = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author page. */
 router.get('/author', function(req, res, next) {
   res.render('author', { author: 'Gonzalo J. Osende Pérez' });
 });
+
+/* GET questions page. */
+router.get('/quizzes', controllers.question);
+
+/* GET results page. */
+router.get('/check', controllers.check);
 
 module.exports = router;

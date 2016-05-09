3c4ea627a2b15bacc42d37a0dc9ec2a1ea2a133d
Gonzalo J. Osende <gonzalojose.osende.perez@alumnos.upm.es>
2016-05-09 18:02:00 +0200

    Autoload quiz

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,17 @@
 var models = require('../models');
 
+// Autoload el quiz asociado a :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(function(quiz) {
+			if(quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId=' + quizId));
+		}
+	}).catch(function(error) { next(error); });
+};
+
 // GET /quizzes
 exports.index = function(req,res,next){
 	var text = req.query.search;
@@ -20,7 +32,7 @@ exports.show = function(req, res, next) {
 	models.Quiz.findById(req.params.id).then(function(quiz) {
 		if(quiz){
 			var answer = req.query.answer || '';
-			res.render('quizzes/show', { quizId: quiz.id, question: quiz.question,
+			res.render('quizzes/show', { quiz: req.quiz,
 									answer: answer });
 		}
 		else {
@@ -34,8 +46,8 @@ exports.check = function(req, res, next) {
 	models.Quiz.findById(req.params.id).then(function(quiz) {
 		if(quiz){
 			var answer = req.query.answer || "";
-  			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-  			res.render('quizzes/result', { quiz: quiz, result: result,
+  			var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+  			res.render('quizzes/result', { quiz: req.quiz, result: result,
   								answer: answer });
 		}
 		else {
--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,15 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-/* GET author page. */
+// Autoload de rutas que usen :quizId
+router.param('id', controllers.load);
+
+// Quizzes
 router.get('/author', function(req, res, next) {
   res.render('author', { author: 'Gonzalo J. Osende Pérez' });
 });
-
 router.get('/quizzes', controllers.index)
-
-/* GET questions page. */
 router.get('/quizzes/:id(\\d+)', controllers.show);
-
-/* GET results page. */
 router.get('/quizzes/:id(\\d+)/check', controllers.check);
 
 module.exports = router;

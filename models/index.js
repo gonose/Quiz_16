4e386b203db810956c480451624e0817b06d48a1
Gonzalo J. Osende <gonzalojose.osende.perez@alumnos.upm.es>
2016-05-27 14:01:23 +0200

    Crear comentarios

--- models/index.js
+++ models/index.js
@@ -17,5 +17,10 @@ var sequelize = new Sequelize(url,
 				{ storage: storage, omitNull: true});
 
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz;
+exports.Comment = Comment

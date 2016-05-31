0251d455a56a42f8d1e90ac6616dde1fd2e5f1e4
Gonzalo J. Osende <gonzalojose.osende.perez@alumnos.upm.es>
2016-05-31 20:09:16 +0200

    MVC de aceptar comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,6 +1,20 @@
 var models = require('../models');
 var Sequelize = require('sequelize');
 
+//Autoload
+exports.load = function(req, res, next, commentId) {
+	models.Comment.findById(commentId)
+		.then(function(comment){
+			if(comment) {
+				req.comment = comment;
+				next();
+			} else {
+				next(new Error('No existe commentId='+commentId));
+			}
+		})
+		.catch(function(error) { next(error); });
+}
+
 //GET /quizzes/:id/comments/new
 exports.new = function(req, res, next){
 	var comment = models.Comment.build({text: ""});
@@ -16,7 +30,7 @@ exports.create = function(req, res ,next){
 	});
 
 	comment.save().then(function(comment) {
-		req.flash('succes', 'Comentario creado con exito.');
+		req.flash('success', 'Comentario creado con exito.');
 		res.redirect('/quizzes/' + req.quiz.id);
 	})
 	.catch(Sequelize.ValidationError, function(error) {
@@ -32,4 +46,18 @@ exports.create = function(req, res ,next){
 		req.flash('error', 'Error al crear el comentario: ' + error.message);
 		next(error);
 	});
+};
+
+// GET /quizzes/:id/comments/:commentId/accept
+exports.accept = function(req, res, next){
+	req.comment.accepted = true;
+	req.comment.save(["accepted"])
+		.then(function(comment) {
+			req.flash('succes', 'Comentario aceptado con éxito.');
+			res.redirect('/quizzes/'+req.params.id);
+		})
+		.catch(function(error) {
+			req.flash('error','Error al aceptar un comentario: '+error.message);
+			next(error);
+		});
 };
--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
 // Autoload de rutas que usen :quizId
 router.param('id', quizcontrollers.load);
 router.param('userId', usercontroller.load);
+router.param('commentId', commentController.load);
 
 // Quizzes
 router.get('/author', function(req, res, next) {
@@ -22,24 +23,36 @@ router.get('/author', function(req, res, next) {
 router.get('/quizzes.:format?', 			quizcontrollers.index);
 router.get('/quizzes/:id(\\d+).:format?', 	quizcontrollers.show);
 router.get('/quizzes/:id(\\d+)/check', 		quizcontrollers.check);
-router.get('/quizzes/new', 					sessioncontroller.loginRequired, quizcontrollers.new);
-router.post('/quizzes', 					sessioncontroller.loginRequired, quizcontrollers.create);
-router.get('/quizzes/:id(\\d+)/edit', 		sessioncontroller.loginRequired, quizcontrollers.edit);
-router.put('/quizzes/:id(\\d+)', 			sessioncontroller.loginRequired, quizcontrollers.update);
-router.delete('/quizzes/:id(\\d+)', 		sessioncontroller.loginRequired, quizcontrollers.destroy);
+router.get('/quizzes/new', 					sessioncontroller.loginRequired, 
+											quizcontrollers.new);
+router.post('/quizzes', 					sessioncontroller.loginRequired, 
+											quizcontrollers.create);
+router.get('/quizzes/:id(\\d+)/edit', 		sessioncontroller.loginRequired, 
+											quizcontrollers.edit);
+router.put('/quizzes/:id(\\d+)', 			sessioncontroller.loginRequired, 
+											quizcontrollers.update);
+router.delete('/quizzes/:id(\\d+)', 		sessioncontroller.loginRequired, 
+											quizcontrollers.destroy);
 
 // Comments
-router.get('/quizzes/:id(\\d+)/comments/new', 	sessioncontroller.loginRequired, commentController.new);
-router.post('/quizzes/:id(\\d+)/comments', 		sessioncontroller.loginRequired, commentController.create)
-
+router.get('/quizzes/:id(\\d+)/comments/new', 	sessioncontroller.loginRequired, 
+												commentController.new);
+router.post('/quizzes/:id(\\d+)/comments', 		sessioncontroller.loginRequired, 
+												commentController.create)
+router.put('/quizzes/:id(\\d+)/comments/:commentId(\\d+)/accept',
+												sessioncontroller.loginRequired, 
+												commentController.accept);
 // Users
 router.get('/users', 						usercontroller.index);
 router.get('/users/:userId(\\d+)',			usercontroller.show);
 router.get('/users/new',					usercontroller.new);
 router.post('/users',						usercontroller.create);
-router.get('/users/:userId(\\d+)/edit',		sessioncontroller.loginRequired, usercontroller.edit);
-router.put('/users/:userId(\\d+)',			sessioncontroller.loginRequired, usercontroller.update);
-router.delete('/users/:userId(\\d+)',		sessioncontroller.loginRequired, usercontroller.destroy);
+router.get('/users/:userId(\\d+)/edit',		sessioncontroller.loginRequired, 
+											usercontroller.edit);
+router.put('/users/:userId(\\d+)',			sessioncontroller.loginRequired, 
+											usercontroller.update);
+router.delete('/users/:userId(\\d+)',		sessioncontroller.loginRequired, 
+											usercontroller.destroy);
 
 // Session
 router.get('/session', sessioncontroller.new);
